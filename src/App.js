e34e3f6da4a51f4263b0bdfecdfb2efc2011d443
shabarish <shabarishkera@gmail.com>
2023-03-20 22:54:15 +0530

    routing setup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import About from "./components/About"
+import Login from './components/Login';
+import Home from './components/Home';
+import {BrowserRouter as Router,
+Routes,Route} from "react-router-dom"
 function App() {
   return (
     <>
+    <Router >
     
+    <Navbar/>
+
+    <Routes>
+<Route exact path="/" element={<Home key="home" />}></Route>
+<Route exact path="/about" element={<About key="about" />}></Route>
+<Route exact path="/login" element={<Login key="login" />}></Route>
+
+
+
+    </Routes>
     
-    
+    </ Router >
     </>
        
   );
