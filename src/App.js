f959f87f5c086c090b7b6c25df70ef0fe89bbe15
shabarish <shabarishkera@gmail.com>
2023-03-21 18:09:05 +0530

    create a context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import About from "./components/About"
 import Login from './components/Login';
 import Home from './components/Home';
+import  NoteContext from "./context/notes/noteconstext";
+import NoteState from './context/notes/notestate';
 import {BrowserRouter as Router,
 Routes,Route} from "react-router-dom"
 function App() {
   return (
     <>
+    <NoteState>
     <Router >
     
     <Navbar/>
@@ -23,6 +26,7 @@ function App() {
     </Routes>
     
     </ Router >
+    </NoteState>
     </>
        
   );
