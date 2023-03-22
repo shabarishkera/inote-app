3fb3232433f4d4fcb3ae70b599e6836d0644874d
shabarish <shabarishkera@gmail.com>
2023-03-22 07:33:20 +0530

    notes displayed

--- src/context/notes/notestate.js
+++ src/context/notes/notestate.js
@@ -17,7 +17,31 @@ var nots=[
         "description": "fddfdfd a description ",
         "date": "2023-03-20T07:30:53.093Z",
         "__v": 0
-      }
+      },
+      {
+          "_id": "64180badc8cb09ff96aa81f5",
+          "user": "64180b2525e3c2369c433f2d",
+          "title": "jack dies",
+          "description": "fddfdfd a description ",
+          "date": "2023-03-20T07:30:53.093Z",
+          "__v": 0
+        },
+        {
+            "_id": "64180badc8cb09ff96aa81f5",
+            "user": "64180b2525e3c2369c433f2d",
+            "title": "jack dies",
+            "description": "fddfdfd a description ",
+            "date": "2023-03-20T07:30:53.093Z",
+            "__v": 0
+          },
+          {
+              "_id": "64180badc8cb09ff96aa81f5",
+              "user": "64180b2525e3c2369c433f2d",
+              "title": "jack dies",
+              "description": "fddfdfd a description ",
+              "date": "2023-03-20T07:30:53.093Z",
+              "__v": 0
+            }
   ]
   const [notes,setNotes]=useState(nots)
 
